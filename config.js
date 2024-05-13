3f4f3e27623be1f0b9b81b5495fdf6bda3aa9e3b
Senkubo <ahmedaligue19@gmail.com>
2024-05-13 13:15:06 +0100

    Update config.js

--- config.js
+++ config.js
@@ -10,19 +10,19 @@ import moment from 'moment-timezone';
 global.botnumber = "*『👑𝑺𝐴𝑅𝐴-𝐵𝛩𝑇👑』*"
 global.confirmCode = ""
 
-global.owner = [['201280244814', '⁨𝐻𝐴𝑅𝐿𝐸𝑌 𝐿𝐼𝐺𝐻𝑇⁩', true],
+global.owner = [[212658594530', ahmed Isighami', true],
 ];
 
-global.suittag = ['201280244814'];
-global.prems = ['201280244814'];
+global.suittag = [212658594530'];
+global.prems = [212658594530'];
 
-global.packname = '𝐻𝐴𝑅𝐿𝐸𝑌 𝐿𝐼𝐺𝐻𝑇⁩';
-global.author = '👑𝑺𝐴𝑅𝐴-𝐵𝛩𝑇👑';
-global.wm = '👑𝑺𝐴𝑅𝐴-𝐵𝛩𝑇👑';
-global.titulowm = '☾︎ 👑𝑺𝐴𝑅𝐴-𝐵𝛩𝑇👑 ☽︎';
-global.titulowm2 = `☾︎ 👑𝑺𝐴𝑅𝐴-𝐵𝛩𝑇👑 ☽︎`
-global.igfg = '☾︎ 👑𝑺𝐴𝑅𝐴-𝐵𝛩𝑇👑 ☽︎';
-global.wait = '*[ ⏳ ] انتظر جاي التحميل تابع صاحب البوت*...\n\n*atom.bio/harley_light*';
+global.packname = '.    Senku     . ';
+global.author = 'senkubot';
+global.wm = '👑senku bot👑';
+global.titulowm = '☾︎ 👑senku-𝐵𝛩𝑇👑 ☽︎';
+global.titulowm2 = `☾︎ 👑senku-𝐵𝛩𝑇👑 ☽︎`
+global.igfg = '☾︎ 👑senku-𝐵𝛩𝑇👑 ☽︎';
+global.wait = '*[ ⏳ ] انتظر جاري التحميل صل على نبينا وحبيبنا محمد *';
 
 global.imagen1 = fs.readFileSync('./Menu2.jpg');
 global.imagen2 = fs.readFileSync('./src/nuevobot.jpg');
@@ -42,15 +42,15 @@ global.mes = d.toLocaleDateString('ar', {month: 'long'});
 global.año = d.toLocaleDateString('ar', {year: 'numeric'});
 global.tiempo = d.toLocaleString('ar-US', {hour: 'numeric', minute: 'numeric', second: 'numeric', hour12: true});
 //* ****************************
-global.wm2 = `${dia} ${fecha}\n☾︎ 👑𝑺𝐴𝑅𝐴-𝐵𝛩𝑇👑 ☽︎`;
-global.gt = '☾︎ 👑𝑺𝐴𝑅𝐴-𝐵𝛩𝑇👑 ☽︎';
-global.mysticbot = '☾︎ 👑𝑺𝐴𝑅𝐴-𝐵𝛩𝑇👑 ☽︎';
-global.md = 'https://chat.whatsapp.com/H93YpO0LkJNHoTmtIhcL1z';
-global.mysticbot = 'atom.bio/harley_light';
-global.waitt = '*[ ⏳ ] انتظر جاي التحميل تابع صاحب البوت*...\n\n*atom.bio/harley_light*';
-global.waittt = '*[ ⏳ ] انتظر جاي التحميل تابع صاحب البوت*...\n\n*atom.bio/harley_light*';
-global.waitttt = '*[ ⏳ ] انتظر جاي التحميل تابع صاحب البوت*...\n\n*atom.bio/harley_light*';
-global.nomorown = '201280244814';
+global.wm2 = `${dia} ${fecha}\n☾︎ 👑senku-𝐵𝛩𝑇👑 ☽︎`;
+global.gt = '☾︎ 👑senku-𝐵𝛩𝑇👑 ☽︎';
+global.mysticbot = '☾︎ 👑senku-𝐵𝛩𝑇👑 ☽︎';
+global.md = 'https://chat.whatsapp.com/ENfYi5dQNCuIJVyfd8TdeP';
+global.mysticbot = '';
+global.waitt = '*[ ⏳ ] انتظر جاي التحميل صل وسلم على نبينا محمد ';
+global.waittt = '*[ ⏳ ] انتظر جاي التحميل صل وسلم على نبينا محمد *';
+global.waitttt = '*[ ⏳ ] انتظر جاي التحميل صل وسلم على نبينا محمد *';
+global.nomorown = 212658594530';
 global.pdoc = ['application/vnd.openxmlformats-officedocument.presentationml.presentation', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'application/vnd.openxmlformats-officedocument.wordprocessingml.document', 'application/vnd.ms-excel', 'application/msword', 'application/pdf', 'text/rtf'];
 global.cmenut = '❖––––––『';
 global.cmenub = '┊✦ ';
